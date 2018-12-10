80cbc563d9c325ae144fe6e190905182ecd4e822
Unknown <gregoiremasse1@hotmail.fr>
2018-12-10 10:55:41 +0100

    MAJ app.module greg

--- monRDV-angular/src/app/app.module.ts
+++ monRDV-angular/src/app/app.module.ts
@@ -4,21 +4,23 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
-import {PatientMesPatientsComponent} from './patient-mes-patients/patient-mes-patients.component';
 import {PatientMesPatientsHttpService} from './patient-mes-patients/patient-mes-patients-http.service';
-import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { PatientCalendrierComponent } from './patient-calendrier/patient-calendrier.component';
 import { InscriptionFormPatientComponent } from './model/inscription-form-patient/inscription-form-patient.component';
 import { PatientDonneesUtilisateurComponent } from './patient-donnees-utilisateur/patient-donnees-utilisateur.component';
-import {PatientDonneesUtilisateurHttpService} from './patient-donnees-utilisateur/patient-donnees-utilisateur-http.service';
+import {PatientRdvInformationHttpService} from './patient-rdv-information/patient-rdv-information-http.service';
+import {PatientRdvInformationComponent} from './patient-rdv-information/patient-rdv-information.component';
+
+
 
 const routes: Routes = [
   {path: 'patient/mesrdvavenir', component: PatientCalendrierComponent},
   {path: 'patient/mesrdvpasses', component: PatientRdvInformationComponent},
-  {path: 'patient/mesinfos', component: [PatientDonneesUtilisateurComponent, PatientMesPatientsComponent]},
+  {path: 'patient/mesinfos', component: PatientDonneesUtilisateurComponent},
   {path: '', redirectTo: 'patient/mesrdvavenir', pathMatch: 'full'},
-]
+
+];
 
 
 @NgModule({
@@ -34,7 +36,7 @@ const routes: Routes = [
     HttpModule,
     RouterModule.forRoot(routes),
   ],
-  providers: [PatientMesPatientsHttpService, PatientRdvInformationHttpService, PatientCalendrierHttpService, PatientDonneesUtilisateurHttpService],
+  providers: [PatientMesPatientsHttpService, PatientRdvInformationHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
