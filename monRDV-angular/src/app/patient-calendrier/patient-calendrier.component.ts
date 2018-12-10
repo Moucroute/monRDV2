2da1e5e4f833b96b0f6893357aa3a1c6b3a721ee
Gonzales <arthur.af.gonzales@gmail.com>
2018-12-10 10:23:03 +0100

    component Patient Calendrier

---
+++ monRDV-angular/src/app/patient-calendrier/patient-calendrier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-calendrier',
+  templateUrl: './patient-calendrier.component.html',
+  styleUrls: ['./patient-calendrier.component.css']
+})
+export class PatientCalendrierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
