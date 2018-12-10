8ec2264d50de98c5beef786198a15bb90b2a73af
SimonV <simon.varvat@gmail.com>
2018-12-10 20:43:55 +0100

    test de Simon ... échec cuisant !

--- monRDV-angular/src/app/app.module.ts
+++ monRDV-angular/src/app/app.module.ts
@@ -1,5 +1,3 @@
-
-
 import {AppComponent} from './app.component';
 import {PatientMesPatientsComponent} from './patient-mes-patients/patient-mes-patients.component';
 import {PatientMesPatientsHttpService} from './patient-mes-patients/patient-mes-patients-http.service';
@@ -11,13 +9,13 @@ import {PatientCalendrierHttpService} from './patient-calendrier/patient-calendr
 import {PatientDonneesUtilisateurHttpService} from './patient-donnees-utilisateur/patient-donnees-utilisateur-http.service';
 import {PatientRdvInformationComponent} from './patient-rdv-information/patient-rdv-information.component';
 import {PatientMesRdvAVenirComponent} from './patient-mes-rdv-a-venir/patient-mes-rdv-a-venir.component';
-import { PatientMesRdvPassesComponent } from './patient-mes-rdv-passes/patient-mes-rdv-passes.component';
-import { PatientMesInfosComponent } from './patient-mes-infos/patient-mes-infos.component';
-import {RouterModule, Routes} from "@angular/router";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {NgModule} from "@angular/core";
+import {PatientMesRdvPassesComponent} from './patient-mes-rdv-passes/patient-mes-rdv-passes.component';
+import {PatientMesInfosComponent} from './patient-mes-infos/patient-mes-infos.component';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
 
 
 const routes: Routes = [
