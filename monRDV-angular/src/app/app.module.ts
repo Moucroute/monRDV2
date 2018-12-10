80c811fe5c8103c083a24df93cdc7ba7c24156b8
charlotte mouden <charlotte.mouden@live.fr>
2018-12-10 10:26:32 +0100

    front end donneesPatient componenet

--- monRDV-angular/src/app/app.module.ts
+++ monRDV-angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PatientDonneesUtilisateurComponent } from './patient-donnees-utilisateur/patient-donnees-utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientDonneesUtilisateurComponent
   ],
   imports: [
     BrowserModule,
