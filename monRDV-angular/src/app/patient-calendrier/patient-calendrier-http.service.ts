afd2dad8b945dcf642d4697d2d7a3fa60dbf0e43
Gonzales <arthur.af.gonzales@gmail.com>
2018-12-10 12:28:32 +0100

    rectif arthur

--- monRDV-angular/src/app/patient-calendrier/patient-calendrier-http.service.ts
+++ monRDV-angular/src/app/patient-calendrier/patient-calendrier-http.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Utilisateur} from '../model/utilisateur';
-import {RendezVous} from '../model/rendezVous';
-import {CreneauDisponible} from '../model/creneauDisponible';
+import {RendezVous} from '../model/rendez-vous';
+import {CreneauDisponible} from '../model/creneau-disponible';
 import {Observable} from 'rxjs/Observable';
 
 
