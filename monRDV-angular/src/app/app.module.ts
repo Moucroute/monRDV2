d49bd18dd20aa92b1ab2f0153034dd151adeec65
vjorda <virginie.jorda@gmail.com>
2018-12-10 11:02:44 +0100

    app module OK

--- monRDV-angular/src/app/app.module.ts
+++ monRDV-angular/src/app/app.module.ts
@@ -5,11 +5,11 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {PatientMesPatientsHttpService} from './patient-mes-patients/patient-mes-patients-http.service';
-import {Routes } from '@angular/router';
-import {RouterModule } from '@angular/router';
-import {PatientCalendrierComponent } from './patient-calendrier/patient-calendrier.component';
-import {InscriptionFormPatientComponent } from './model/inscription-form-patient/inscription-form-patient.component';
-import {PatientDonneesUtilisateurComponent } from './patient-donnees-utilisateur/patient-donnees-utilisateur.component';
+import {Routes} from '@angular/router';
+import {RouterModule} from '@angular/router';
+import {PatientCalendrierComponent} from './patient-calendrier/patient-calendrier.component';
+import {InscriptionFormPatientComponent} from './model/inscription-form-patient/inscription-form-patient.component';
+import {PatientDonneesUtilisateurComponent} from './patient-donnees-utilisateur/patient-donnees-utilisateur.component';
 import {PatientDonneesUtilisateurHttpService} from './patient-donnees-utilisateur/patient-donnees-utilisateur-http.service';
 import {PatientRdvInformationComponent} from './patient-rdv-information/patient-rdv-information.component';
 import {PatientRdvInformationHttpService} from './patient-rdv-information/patient-rdv-information-http.service';
@@ -20,9 +20,9 @@ import {PatientCalendrierHttpService} from './patient-calendrier/patient-calendr
 const routes: Routes = [
   {path: 'patient/mesrdvavenir', component: PatientCalendrierComponent},
   {path: 'patient/mesrdvpasses', component: PatientRdvInformationComponent},
-  {path: 'patient/mesinfos', component: [PatientDonneesUtilisateurComponent, PatientMesPatientsComponent]},
+  {path: 'patient/mesinfos', component: PatientDonneesUtilisateurComponent},
   {path: '', redirectTo: 'patient/mesrdvavenir', pathMatch: 'full'}
-]
+];
 
 
 @NgModule({
