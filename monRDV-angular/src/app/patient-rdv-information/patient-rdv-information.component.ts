89e14667d639758abf960af5c15d49ee7ce41262
Unknown <gregoiremasse1@hotmail.fr>
2018-12-10 10:42:29 +0100

    Greg component

---
+++ monRDV-angular/src/app/patient-rdv-information/patient-rdv-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-rdv-information',
+  templateUrl: './patient-rdv-information.component.html',
+  styleUrls: ['./patient-rdv-information.component.css']
+})
+export class PatientRdvInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
