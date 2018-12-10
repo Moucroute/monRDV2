dde183af972d8055c47b93a9fc04b5abea2e9b94
vjorda <virginie.jorda@gmail.com>
2018-12-10 10:58:38 +0100

    Auto stash before merge of "master" and "origin/master"
    ok app module

--- monRDV-angular/src/app/app.module.ts
+++ monRDV-angular/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {PatientMesPatientsHttpService} from './patient-mes-patients/patient-mes-patients-http.service';
-import {RouterModule, Routes} from '@angular/router';
-import { PatientCalendrierComponent } from './patient-calendrier/patient-calendrier.component';
-import { InscriptionFormPatientComponent } from './model/inscription-form-patient/inscription-form-patient.component';
-import { PatientDonneesUtilisateurComponent } from './patient-donnees-utilisateur/patient-donnees-utilisateur.component';
-import {PatientRdvInformationHttpService} from './patient-rdv-information/patient-rdv-information-http.service';
+import {Routes } from '@angular/router';
+import {RouterModule } from '@angular/router';
+import {PatientCalendrierComponent } from './patient-calendrier/patient-calendrier.component';
+import {InscriptionFormPatientComponent } from './model/inscription-form-patient/inscription-form-patient.component';
+import {PatientDonneesUtilisateurComponent } from './patient-donnees-utilisateur/patient-donnees-utilisateur.component';
+import {PatientDonneesUtilisateurHttpService} from './patient-donnees-utilisateur/patient-donnees-utilisateur-http.service';
 import {PatientRdvInformationComponent} from './patient-rdv-information/patient-rdv-information.component';
-import {PatientDonneesUtilisateurService} from './patient-donnees-utilisateur/patient-donnees-utilisateur.service';
+import {PatientRdvInformationHttpService} from './patient-rdv-information/patient-rdv-information-http.service';
+import {PatientCalendrierHttpService} from './patient-calendrier/patient-calendrier-http.service';
 
 
 
@@ -37,7 +39,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
   ],
   providers: [PatientMesPatientsHttpService, PatientRdvInformationHttpService,
-    PatientCalendrierHttpService, PatientDonneesUtilisateurService],
+    PatientCalendrierHttpService, PatientDonneesUtilisateurHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
