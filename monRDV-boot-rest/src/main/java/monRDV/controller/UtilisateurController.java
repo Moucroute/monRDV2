d25d8ef5530cf0ad48a4e1902d141db92f2bea1f
Magnant Jérôme <jermagnant@gmail.com>
2018-12-11 15:14:23 +0100

    connexion

--- monRDV-boot-rest/src/main/java/monRDV/controller/UtilisateurController.java
+++ monRDV-boot-rest/src/main/java/monRDV/controller/UtilisateurController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
-import monRDV.model.Patient;
 import monRDV.model.Utilisateur;
 import monRDV.model.Views;
 import monRDV.repository.IRepositoryUtilisateur;
@@ -36,13 +35,13 @@ public class UtilisateurController {
 	public  Utilisateur connexion(@PathVariable("email") String email,@PathVariable("motDePasse") String motDePasse){
 		
 
-			if((repoUtilisateur.findMotDePasse(motDePasse)!= null) && (repoUtilisateur.findUtilisateur(email) != null)) {
-				return repoUtilisateur.findUtilisateur(email);
-				}
+//			if((repoUtilisateur.findMotDePasse(motDePasse)!= null) && (repoUtilisateur.findUtilisateur(email) != null)) {
+//				return repoUtilisateur.findUtilisateur(email);
+//				}
 		
 		
 	
-			return null;
+			return repoUtilisateur.findUtilisateurByEmailAndMotDePasse(email, motDePasse);
 	
 
 
--- monRDV-boot-rest/src/main/java/monRDV/repository/IRepositoryUtilisateur.java
+++ monRDV-boot-rest/src/main/java/monRDV/repository/IRepositoryUtilisateur.java
@@ -29,4 +29,7 @@ public interface IRepositoryUtilisateur extends JpaRepository<Utilisateur, Long>
 	
 	@Query("select u from Utilisateur u ")
 	public Utilisateur findUser();
+	
+	@Query("select u from Utilisateur u where u.email = :email and u.motDePasse = :motDePasse")
+	public Utilisateur findUtilisateurByEmailAndMotDePasse(@Param("email") String email, @Param("motDePasse") String motDePasse);
 }
